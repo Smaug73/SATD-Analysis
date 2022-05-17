9706c3d7adb2ddc6ee560c294e6226daf0d2fb03
Kateryna Romanyuk <kateryna.romanyuk20@gmail.com>
2022-05-17 17:47:19 +0200

    Added script for converting checkstyle results from xml to csv format.
    Co-authored-by: Stefano Rondinella <Smaug73@users.noreply.github.com>

--- Preprocessing/pathConverter.py
+++ Preprocessing/pathConverter.py
@@ -9,10 +9,7 @@ def add_columns_and_convert_paths(directory, tool):
     # Get the name of each project in the directory
     projects = next(os.walk(directory))[1]
 
-    # Traverse all the project directories
     for project in projects:
-
-        # Create the url to the repository
         repository = 'https://github.com/apache/' + project
 
         # Get the hash of each commit in the project directory
@@ -20,28 +17,21 @@ def add_columns_and_convert_paths(directory, tool):
 
         print('\nProject: ' + project)
 
-        # Traverse all the commit directories
         for commit in commits:
-            print('Commit: ' + commit)
+            print('\nCommit: ' + commit)
 
-            # Read csv file and convert to a pandas DataFrame
             csv_violations = pd.read_csv(directory + '/' + project + '/' + commit + '/' + tool + '-' + commit + '.csv')
 
             # Check if there is any entry
-            # Add the commit to commit_to_analyse list if there is at least one entry in the csv file
+            # If there is at least one entry in the CSV, use PyDriller to get the original path of each file in the CSV
             # Add columns 'Project' and 'Commit' to each CSVs in the directory
             if csv_violations.size != 0:
                 csv_violations['Project'] = project
                 csv_violations['Commit'] = commit
 
-                # Get the commit from remote repository
                 for commit_to_analyse in Repository(repository, single=commit).traverse_commits():
-                    print('\nCommit under analysis: ' + commit_to_analyse.hash)
-
-                    # List of paths of the files modified in the current commit
                     modified_files_list= []
 
-                    # Populate the list
                     for modified_file in commit_to_analyse.modified_files:
                         modified_files_list.append(modified_file.new_path)
                         
@@ -51,26 +41,23 @@ def add_columns_and_convert_paths(directory, tool):
                     # Index for accessing the rows of the CSVs
                     i = 0
 
-                    # Iterate over all the files in the csv file
                     for violation_file in csv_violations['File']:
-
-                        # Get the package of the file
-                        package = csv_violations.at[i, 'Package']
-
-                        # Replace the character '.' with '/' in order to create the path of the file
-                        package = package.replace('.', '/')
-                        i = i + 1
 
                         # Get the name of the file
                         violation_file_name = violation_file.split('/')[-1]
 
-                        # Create the subpath of the file in the CSV
-                        violation_file_subpath = package + '/' + violation_file_name
+                        # If there is a column "Package" in the CSV, use it to create the subpath of the file
+                        if 'Package' in csv_violations.columns:
+                            package = csv_violations.at[i, 'Package']
+                            i = i + 1
 
-                        # Check whether the file was already searched
+                            # Create the subpath of the file in the CSV
+                            package = package.replace('.', '/')
+                            violation_file_subpath = package + '/' + violation_file_name
+                        else:
+                            violation_file_subpath = violation_file_name
+
                         if not(violation_file_subpath in to_exclude):
-
-                            # Add the file to the to_exclude array
                             to_exclude.append(violation_file_subpath)
 
                             # Flag: if the file was already found, exit the loop
@@ -90,16 +77,13 @@ def add_columns_and_convert_paths(directory, tool):
                             
                             if not(found):
                                 print('File ' + violation_file_name + ' was not modified in this commit')
-
-                    # Save all the changes
-                    csv_violations.to_csv(directory + '/' + project + '/' + commit + '/new-' + tool + '-' + commit + '.csv')
+                                
             else:
                 csv_violations['Project'] = ''
                 csv_violations['Commit'] = ''
-                csv_violations.to_csv(directory + '/' + project + '/' + commit + '/new-' + tool + '-' + commit + '.csv')
 
+            csv_violations.to_csv(directory + '/' + project + '/' + commit + '/new-' + tool + '-' + commit + '.csv', index=False)
         
-
 
 if __name__ == "__main__":
 
---
+++ Preprocessing/xmlToCsvConverter.py
@@ -0,0 +1,74 @@
+from xml.dom import minidom
+import pandas as pd
+import argparse
+import os
+
+# Function for explore dataset
+def convert_all_files(directory):
+     # Get the name of each project in the directory
+    projects = next(os.walk(directory))[1]
+
+    for project in projects:
+
+        # Get the hash of each commit in the project directory
+        commits = next(os.walk(directory + '/' + project))[1]
+
+        for commit in commits:
+            print('Commit: ' + commit)
+            filepath = directory + '/' + project + '/' + commit + '/checkstyle-' + commit + '.xml'
+            convert_file(filepath)
+
+
+# Fuction for convert xml to csv
+def convert_file(filepath):
+    
+    checkstyle_csv = pd.DataFrame(columns=['File', 'Line', 'Severity', 'Message', 'Source'])
+
+    # Parse an xml file by name
+    file_xml = minidom.parse(filepath)
+
+    # Parse all file elements
+    files = file_xml.getElementsByTagName('file')
+
+    i = 0
+    for file in files:
+        print("\nFile name: " + file.getAttribute('name'))
+
+        # Parse the errors of a file
+        errors = file.getElementsByTagName('error')
+        
+        # Create a new row and append it to the DataFrame
+        for error in errors:
+            checkstyle_csv.loc[i] = [file.getAttribute('name'), 
+                                    error.getAttribute('line'), 
+                                    error.getAttribute('severity'), 
+                                    error.getAttribute('message'), 
+                                    error.getAttribute('source')]
+            i = i + 1
+
+    filepath_csv = filepath.split('.')[0] + '.csv'
+    checkstyle_csv.to_csv(filepath_csv, index=False)
+
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser(
+    description='Program for converting xml file from checkstyle analysis to csv')
+
+    # Directory of projects being analysed
+    parser.add_argument(
+        'directory_projects',
+        type=str,
+        help='Projects directory',
+    )
+
+    # Parsing the args
+    args = parser.parse_args()
+
+    # Projects Directory
+    pathProjects = args.directory_projects
+    
+    convert_all_files(pathProjects)
+    
+    
+
