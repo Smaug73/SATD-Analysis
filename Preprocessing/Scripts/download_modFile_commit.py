499f733730b78d5a05f9e09cb310aeae0675d212
Smaug73 <stefrondinella@live.it>
2022-05-30 14:29:05 +0200

    Add script for download java file from repo

--- Preprocessing/Scripts/download_modFile_commit.py
+++ Preprocessing/Scripts/download_modFile_commit.py
@@ -1,63 +1,168 @@
-# Example and alpha version of script for download the modified file of
-# a specific commit for a Repository
-
-
 import os
 from pdb import main
 import re
-
 from pydriller import Repository
 
 
+# dir for store projects fix
+dir_repo_fix = '../../Repository-fix/'
+
+
+
+# function for mkdir for store the file
+def mkdir_for_fix(project,commit_id):
+
+    print(f"Creating dir for Project : {str(project)}    Commit: {str(commit_id)}")
+
+    # directory for the commit
+    dir_commit = dir_repo_fix + project + os.sep + commit_id
+
+    # create the fix projects directory if it does not exist, otherwise skip it
+    if os.path.isdir(dir_repo_fix) is False:
+        os.mkdir(dir_repo_fix)
+
+    # create the project directory if it does not exist, otherwise skip it
+    if os.path.isdir(dir_repo_fix + project) is False:
+        os.mkdir(dir_repo_fix + project)
+
+    # create the commitID directory if it does not exist, otherwise skip it
+    if os.path.isdir(dir_commit) is False:
+        os.mkdir(dir_commit)
+    # else:
+    #     # print('No need to save files from commit: ' + commit_id)
+    #     continue
+
+    #return the path where store the files to fix for the commit of the project
+    return dir_commit
 
 
 
 
-commit_id = "0a1b46ce0d436002e8abf9ae74a7ba7324fe093e"
-project = "tinkerpop"
 
-repository = 'https://github.com/apache/' + project
 
-# create the git rep object
-git_rep = Repository(repository, single=commit_id).traverse_commits()
+# Function for download specific file from a commit
+def download_file_from_commit(project,commit_id,file_name):
 
-print('Commit:', commit_id)
+    print('Download the file for the fix')
 
-# directory for the commit
-dir_commits = "/home/stefano/TesiMagistrale/ProvePydriller/"
+    repository = 'https://github.com/apache/' + project
 
-# directory for the commit
-dir_commit = dir_commits + project + '/' + commit_id
+    print('Commit:  '+ commit_id)
 
-# create the project directory if it does not exist, otherwise skit it
-if os.path.isdir(dir_commits + project) is False:
-    os.mkdir(dir_commits + project)
+    '''
+    # directory for the commit
+    dir_commit = dir_repo_fix + project + os.sep + commit_id
 
-# create the commitID directory if it does not exist, otherwise skit it
-if os.path.isdir(dir_commit) is False:
-    os.mkdir(dir_commit)
-# else:
-#     # print('No need to save files from commit: ' + commit_id)
-#     continue
+    # create the project directory if it does not exist, otherwise skip it
+    if os.path.isdir(dir_repo_fix + project) is False:
+        os.mkdir(dir_repo_fix + project)
 
-for commit in git_rep:
+    # create the commitID directory if it does not exist, otherwise skip it
+    if os.path.isdir(dir_commit) is False:
+        os.mkdir(dir_commit)
+    # else:
+    #     # print('No need to save files from commit: ' + commit_id)
+    #     continue
 
-    # iterate over each modified file within the commit
-    for m in commit.modified_files:
+    
 
-        # only proceed with it is a Java file
-        if re.search('\\.java$', m.filename, re.IGNORECASE):
+    dir_commit = mkdir_for_fix(project,commit_id)
 
-            # print(modified_file.filename)
-            # print(modified_file.source_code)
+    # Check if the file were already downloaded
+    if file_name in os.listdir(dir_commit):
+        return
+    
+    '''
 
-            # save the file
-            if m.source_code is not None:
+    # create the git rep object
+    git_rep = Repository(repository, single=commit_id).traverse_commits()
 
-                file = open(dir_commit + '/' + m.filename, "w", encoding="utf-8")
-                file.write(m.source_code)
-                file.close()
 
-print('Files saved from commit: ' + commit_id)
+    for commit in git_rep:
 
-print('Done downloading files from project:', project)
+        # iterate over each modified file within the commit
+        for m in commit.modified_files:
+            
+            if(m.filename == file_name):
+                download_Modifiedfile(m,project,commit)
+
+            '''
+            # only proceed with it is a Java file with the searched name
+            # TO DO : DIFFERENT CASE OF MATCH FOR PMD AND CHECKSTYLE
+            #if re.search('\\.java$', m.filename, re.IGNORECASE):
+            if re.search('\\'+file_name+'.java$', m.filename, re.IGNORECASE):
+
+                print('File found:  ',m.filename)
+                # print(modified_file.source_code)
+
+                # save the file
+                if m.source_code is not None:
+
+                    file = open(dir_commit + '/' + m.filename, "w", encoding="utf-8")
+                    file.write(m.source_code)
+                    file.close()
+            '''
+
+
+
+
+# Function for download file from ModifiedFile
+def download_Modifiedfile(mod_file,project,commit):
+    
+    print(f'Downloading file:  {mod_file.filename} ...')
+
+    try:
+        # mkdir for store the file
+        dir_commit = mkdir_for_fix(project,commit)
+
+        # Check if the file were already downloaded
+        if mod_file.filename in os.listdir(dir_commit):
+            return
+
+        print('File found:  ',mod_file.filename)
+        # print(modified_file.source_code)
+
+        # save the file
+        if mod_file.source_code is not None:
+
+            file = open(dir_commit + os.sep() + mod_file.filename, "w", encoding="utf-8")
+            file.write(mod_file.source_code)
+            file.close()
+
+            print(f'File:  {mod_file.filename}    Downloaded! ')
+            print(f'FilePath:  {dir_commit + os.sep() + mod_file.filename}    Downloaded! ')
+        
+        else:
+            print(f'File:  {mod_file.filename}    NOT DOWNLOADED! ')
+    
+    except Exception as e:
+        print(e)
+
+
+
+    
+
+
+if __name__ == "__main__":
+
+
+    # TEST ################################# 
+    
+    commit_id = "0a1b46ce0d436002e8abf9ae74a7ba7324fe093e"
+    project = "tinkerpop"
+
+
+    repository = 'https://github.com/apache/' + project
+
+    # create the git rep object
+    git_rep = Repository(repository, single=commit_id).traverse_commits()
+
+    for commit in git_rep:
+
+        # iterate over each modified file within the commit
+        for m in commit.modified_files:
+            
+            # test for all the file of the commit
+            download_Modifiedfile(m,project,commit.hash)
+
+    #################################
