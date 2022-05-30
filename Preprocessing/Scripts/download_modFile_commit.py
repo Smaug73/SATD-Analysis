d39e151e2dacaeef5c2d7d604d64ef69330e147d
Smaug73 <stefrondinella@live.it>
2022-05-30 15:14:52 +0200

    Fix save modfile

--- Preprocessing/Scripts/download_modFile_commit.py
+++ Preprocessing/Scripts/download_modFile_commit.py
@@ -2,38 +2,44 @@ import os
 from pdb import main
 import re
 from pydriller import Repository
+from pydriller import ModifiedFile
 
 
 # dir for store projects fix
-dir_repo_fix = '../../Repository-fix/'
+dir_repo_fix = ".."+os.sep+".."+os.sep+"Repository-fix"+os.sep
 
 
 
 # function for mkdir for store the file
 def mkdir_for_fix(project,commit_id):
-
+    
     print(f"Creating dir for Project : {str(project)}    Commit: {str(commit_id)}")
 
-    # directory for the commit
-    dir_commit = dir_repo_fix + project + os.sep + commit_id
+    try:
+    
+        # directory for the commit
+        dir_commit = dir_repo_fix + project + os.sep + commit_id
 
-    # create the fix projects directory if it does not exist, otherwise skip it
-    if os.path.isdir(dir_repo_fix) is False:
-        os.mkdir(dir_repo_fix)
+        # create the fix projects directory if it does not exist, otherwise skip it
+        if os.path.isdir(dir_repo_fix) is False:
+            os.mkdir(dir_repo_fix)
 
-    # create the project directory if it does not exist, otherwise skip it
-    if os.path.isdir(dir_repo_fix + project) is False:
-        os.mkdir(dir_repo_fix + project)
+        # create the project directory if it does not exist, otherwise skip it
+        if os.path.isdir(dir_repo_fix + project) is False:
+            os.mkdir(dir_repo_fix + project)
 
-    # create the commitID directory if it does not exist, otherwise skip it
-    if os.path.isdir(dir_commit) is False:
-        os.mkdir(dir_commit)
-    # else:
-    #     # print('No need to save files from commit: ' + commit_id)
-    #     continue
+        # create the commitID directory if it does not exist, otherwise skip it
+        if os.path.isdir(dir_commit) is False:
+            os.mkdir(dir_commit)
+        # else:
+        #     # print('No need to save files from commit: ' + commit_id)
+        #     continue
 
-    #return the path where store the files to fix for the commit of the project
-    return dir_commit
+        #return the path where store the files to fix for the commit of the project
+        return dir_commit
+        
+    except Exception as e:
+        print(e)
 
 
 
@@ -41,73 +47,35 @@ def mkdir_for_fix(project,commit_id):
 
 
 # Function for download specific file from a commit
-def download_file_from_commit(project,commit_id,file_name):
+def download_file_from_commit(project : str ,commit_id : str ,file_name : str):
 
     print('Download the file for the fix')
-
-    repository = 'https://github.com/apache/' + project
-
-    print('Commit:  '+ commit_id)
-
-    '''
-    # directory for the commit
-    dir_commit = dir_repo_fix + project + os.sep + commit_id
-
-    # create the project directory if it does not exist, otherwise skip it
-    if os.path.isdir(dir_repo_fix + project) is False:
-        os.mkdir(dir_repo_fix + project)
-
-    # create the commitID directory if it does not exist, otherwise skip it
-    if os.path.isdir(dir_commit) is False:
-        os.mkdir(dir_commit)
-    # else:
-    #     # print('No need to save files from commit: ' + commit_id)
-    #     continue
-
     
+    try:
+        repository = 'https://github.com/apache/' + project
 
-    dir_commit = mkdir_for_fix(project,commit_id)
+        print('Commit:  '+ commit_id)
 
-    # Check if the file were already downloaded
-    if file_name in os.listdir(dir_commit):
-        return
-    
-    '''
-
-    # create the git rep object
-    git_rep = Repository(repository, single=commit_id).traverse_commits()
+        # create the git rep object
+        git_rep = Repository(repository, single=commit_id).traverse_commits()
 
 
-    for commit in git_rep:
+        for commit in git_rep:
 
-        # iterate over each modified file within the commit
-        for m in commit.modified_files:
-            
-            if(m.filename == file_name):
-                download_Modifiedfile(m,project,commit)
+            # iterate over each modified file within the commit
+            for m in commit.modified_files:
+                
+                if(m.filename == file_name):
+                    download_Modifiedfile(m,project,commit)
 
-            '''
-            # only proceed with it is a Java file with the searched name
-            # TO DO : DIFFERENT CASE OF MATCH FOR PMD AND CHECKSTYLE
-            #if re.search('\\.java$', m.filename, re.IGNORECASE):
-            if re.search('\\'+file_name+'.java$', m.filename, re.IGNORECASE):
-
-                print('File found:  ',m.filename)
-                # print(modified_file.source_code)
-
-                # save the file
-                if m.source_code is not None:
-
-                    file = open(dir_commit + '/' + m.filename, "w", encoding="utf-8")
-                    file.write(m.source_code)
-                    file.close()
-            '''
+    except Exception as e:
+        print(e)
 
 
 
 
 # Function for download file from ModifiedFile
-def download_Modifiedfile(mod_file,project,commit):
+def download_Modifiedfile(mod_file : ModifiedFile,project : str ,commit: str):
     
     print(f'Downloading file:  {mod_file.filename} ...')
 
@@ -126,13 +94,13 @@ def download_Modifiedfile(mod_file,project,commit):
         if mod_file.source_code is not None:
 
             # Rename the file with the path because of the possible re-writing of file with same name 
-            file = open(dir_commit + '/' + mod_file.new_path.replace('/','#'), "w", encoding="utf-8")
+            file = open(dir_commit + os.sep + mod_file.new_path.replace('/','#'), "w", encoding="utf-8")
 
             file.write(mod_file.source_code)
             file.close()
 
             print(f'File:  {mod_file.filename}    Downloaded! ')
-            print(f'FilePath:  {dir_commit + os.sep() + mod_file.filename}    Downloaded! ')
+            print(f'FilePath:  {dir_commit + os.sep + mod_file.filename}    Downloaded! ')
         
         else:
             print(f'File:  {mod_file.filename}    NOT DOWNLOADED! ')
@@ -148,7 +116,7 @@ def download_Modifiedfile(mod_file,project,commit):
 if __name__ == "__main__":
 
 
-    # TEST ################################# 
+    # TEST download_Modifiedfile ################################# 
     
     commit_id = "0a1b46ce0d436002e8abf9ae74a7ba7324fe093e"
     project = "tinkerpop"
