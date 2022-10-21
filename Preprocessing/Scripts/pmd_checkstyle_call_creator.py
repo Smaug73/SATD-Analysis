058684709bc3219f080a9280c88300e04e5bca64
Smaug73 <stefrondinella@live.it>
2022-10-21 12:57:40 +0200

    Script for call end

--- Preprocessing/Scripts/pmd_checkstyle_call_creator.py
+++ Preprocessing/Scripts/pmd_checkstyle_call_creator.py
@@ -15,149 +15,64 @@ dir_pmd = '../pmd-bin-6.45.0/'
 dir_checkstyle = '../checkstyle/'
 
 
+
+
+
 # Launch Checkstyle
-def checkstyle_call():
-
-    commit_id = "0a1b46ce0d436002e8abf9ae74a7ba7324fe093e"
-
-    project = "tinkerpop"
-
-    # the local dir where the java files are located
-    dir_local_path = "/home/stefano/TesiMagistrale/ProvePydriller/"+project+"/"
-
-
-    #### MakeDir for the analysis in the CheckStyle directory #######
-    #### ...SATD_Analysis/Checkstyle/repository/*allrepository/*allcommits
-
-    
-
-    dir_project_output= dir_checkstyle+'repository'+ os.sep +project
-
-    # create the project directory if it does not exist, otherwise skit it
-    if os.path.isdir(dir_project_output) is False:
-        os.mkdir(dir_project_output)
-
-    dir_project_commit_output= dir_project_output+ '/'+commit_id
-
-    # create the commitID directory if it does not exist, otherwise skit it
-    if os.path.isdir(dir_project_commit_output) is False:
-        os.mkdir(dir_project_commit_output)
-
-    #######################
-
-
-
-    print('Started creating call for ' + project)
+def checkstyle_call(file_path,output_path):
 
     # java -jar checkstyle-8.36-all.jar -c checkstyle-all-checks_v8.36.xml -f xml -o
 
     #   FIX DA FARE : il file di configurazione va indicato in quale cartella si trova
-    checkstyle = 'java -jar '+dir_checkstyle+'checkstyle-10.2-all.jar -c google_checkstyle_configuration.xml -f xml -o '
+    checkstyle = 'java -jar '+dir_checkstyle+'checkstyle-10.2-all.jar -c '+dir_checkstyle+'google_checkstyle_configuration.xml -f xml -o '
 
-    
-    # iterate over the project comments and save call string
-    for root, dirs, files in os.walk(dir_local_path, topdown=False):
+    output_path = output_path.replace('java', '') + 'xml'
 
-        # iterate over the files of the commit
-        for file in files:
+    call = checkstyle + output_path + ' ' + file_path
 
-            # just proceed if it is a Java file
-            if '.java' in file:
+    # lanch checkstyle for the specific file
+    try:
+        print()
+        print('Launch Checkstyle: '+call)
+        command= call
+        p=subprocess.Popen(command.split(),stdout=subprocess.PIPE)
+        p.wait()
+        print('Done.. ')
+    except Exception:
+        print("Errore lancio {}".format(command))
+        print(Exception)
 
-                path = dir_local_path
-
-                output = dir_project_commit_output + os.sep +'checkstyle-' + file.replace('java', '') + 'xml'
-
-                folder = path + commit_id + os.sep + file
-
-                call = checkstyle + output + ' ' + folder
-            
-                # lanch checkstyle for the specific file
-                try:
-                    print()
-                    print('Launch Checkstyle: '+call)
-                    command= call
-                    p=subprocess.Popen(command.split(),stdout=subprocess.PIPE)
-                    p.wait()
-                    print('Done.. ')
-                except Exception:
-                    print("Errore lancio {}".format(command))
-                    print(Exception)
-
-    print('\nDone with project ' + project)
 
 
 
 
 
 # Launch PMD
-def pmd_call():
+def pmd_call(file_path,output_path):
 
-    commit_id = "0a1b46ce0d436002e8abf9ae74a7ba7324fe093e"
+    output_path = output_path.replace('java', '') + 'csv'
 
-    project = "tinkerpop"
+    #call = dir_pmd+'pmd-bin-6.45.0/bin/run.sh pmd -d '+ folder + ' -R rulesets/java/quickstart.xml -f csv --no-cache > ' + output
+    call = dir_pmd+'pmd-bin-6.50.0/bin/run.sh pmd -d '+ file_path + ' -R rulesets/java/quickstart.xml -f csv --no-cache ' 
 
-    # the local dir where the java files are located
-    dir_local_path = "/home/stefano/TesiMagistrale/ProvePydriller/"+project+"/"
-
-
-    #### MakeDir for the analysis in the PMD directory #######
-    #### ...SATD_Analysis/PMD/repository/*allrepository/*allcommits
-
-    dir_pmd_rep = dir_pmd+'repository'
-    # create the project directory if it does not exist, otherwise skit it
-    if os.path.isdir(dir_pmd_rep) is False:
-        os.mkdir(dir_pmd_rep)
-
-    dir_project_output= dir_pmd_rep+ os.sep +project
-
-    # create the project directory if it does not exist, otherwise skit it
-    if os.path.isdir(dir_project_output) is False:
-        os.mkdir(dir_project_output)
-
-    dir_project_commit_output= dir_project_output+ '/'+commit_id
-
-    # create the commitID directory if it does not exist, otherwise skit it
-    if os.path.isdir(dir_project_commit_output) is False:
-        os.mkdir(dir_project_commit_output)
-
-    #######################
-
-    # iterate over the project comments and save call string
-    for root, dirs, files in os.walk(dir_local_path, topdown=False):
-
-        # iterate over the files of the commit
-        for file in files:
-
-             # just proceed if it is a Java file
-            if '.java' in file:
-
-                path = dir_local_path
-
-                output = dir_project_commit_output + os.sep +'pmd-' + file.replace('java', '') + 'csv'
-
-                folder = path + commit_id + os.sep + file
-
-                #call = dir_pmd+'pmd-bin-6.45.0/bin/run.sh pmd -d '+ folder + ' -R rulesets/java/quickstart.xml -f csv --no-cache > ' + output
-                call = dir_pmd+'pmd-bin-6.50.0/bin/run.sh pmd -d '+ folder + ' -R rulesets/java/quickstart.xml -f csv --no-cache'
-
-
-                # lanch PMD for the specific file
-                try:
-                    print()
-                    print('Launch PMD: '+call)
-                    command= call
-                    #p=subprocess.Popen(command.split(),stdout=subprocess.PIPE)
-                    p=subprocess.Popen(command.split(),stdout=open(output,'w'))
-                    p.wait()
-                    print('Done.. ')
-                except Exception:
-                    print("Errore lancio {}".format(command))
-                    print(Exception)
+    # lanch PMD for the specific file
+    try:
+        print()
+        print('Launch PMD: '+call)
+        command= call
+        #p=subprocess.Popen(command.split(),stdout=subprocess.PIPE)
+        p=subprocess.Popen(command.split(),stdout=open(output_path,'w'))
+        p.wait()
+        print('Done.. ')
+    except Exception:
+        print("Errore lancio {}".format(command))
+        print(Exception)
 
 
 
 
+
+#   Funzione per scorrere tutti i file da analizzare
 def read_rep(reps_path):
 
     #   ogni repository ha una cartella il cui nome è il commit_id di riferimento
@@ -185,7 +100,7 @@ def read_rep(reps_path):
         if os.path.isdir(reps_path+repo):
             # per ogni repository creiamo cartella di output dei risultati
             dir_pmd_output= dir_pmd_rep+ os.sep + repo
-
+            
             #   create the project directory if it does not exist, otherwise skit it
             if os.path.isdir(dir_pmd_output) is False:
                 os.mkdir(dir_pmd_output)
@@ -219,17 +134,20 @@ def read_rep(reps_path):
 
                     # per ogni file nel commit eseguiamo analisi pmd e checkstyle
                     for file in os.listdir(reps_path+os.sep+repo+os.sep+commit):
+
+                        file_path = reps_path+os.sep+repo+os.sep+commit+os.sep+file
                         
-                        if(os.path.isfile(file) and '.java' in file) :
+                        if(os.path.isfile(file_path) and '.java' in file) :
                             
-                            print(file)
+                            print('Analysis for : '+file)
 
                             #launch pmd 
+                            pmd_call(file_path , commit_pmd_output+os.sep+file)
                             
                             #launch checkstyle
+                            checkstyle_call(file_path , commit_pmd_output+os.sep+file)
 
-
-
+                            print()
 
 
 
