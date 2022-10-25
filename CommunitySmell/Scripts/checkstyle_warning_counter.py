308bc4cc4496d79281dda6da52661d0762d2fa3f
Smaug73 <stefrondinella@live.it>
2022-10-25 18:52:56 +0200

    checkstyle warning counter

---
+++ CommunitySmell/Scripts/checkstyle_warning_counter.py
@@ -0,0 +1,3 @@
+#   Script per il conteggio dei warning all'interno di checkstyle e generazione di csv
+#   Il conteggio va fatto a livello di metodo, per capire se un warning si trova all'interno di un metodo
+#   va visto l'informazione sulle line dal dataset costruito con pydriller e la line del warning dal xml di checkstyle
